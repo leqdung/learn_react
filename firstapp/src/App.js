3a1e518b7146bc72f1badd6db486b9fcd4f490b2
leqdung <dzung.dg@gmail.com>
2022-10-15 00:16:08 +0700

    thay các giá trị tại vị trí khác nhau bằng việc truyền dữ liệu thông dùng props

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -25,15 +25,37 @@ function Feature() {
     </div>
   );
 }
-
+//App là  parent component: trong quan hệ cha con liên quan đến props
 function App() {
   return (
     <div>
-      <Feature /> {/**gọi 1 component ra  */}
-      <Feature /> {/**gọi 1 component ra  */}
-      <Feature /> {/**gọi 1 component ra  */}
+      <YoutubeItem
+        image="https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        avatar="https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        title="Title awesom"
+        author="LeQDung"
+      />
     </div>
   );
 }
 
+//minh họa props bằng youtube item component
+// mục đích của props là để thay đổi ảnh bìa, tên người, title,avatar ==> props sinh ra để được truyền vào chỗ cần thay đổi trong component
+function YoutubeItem(props) {
+  console.log(props);
+  return (
+    <div className="youtube__item">
+      <div className="youtube__image">
+        <img src={props.image} alt="youtube image" />
+      </div>
+      <div className="youtube__footer">
+        <img src={props.avatar} alt="" className="youtube__avata" />
+        <div className="youtube__infor">
+          <h3 className="youtube__title">{props.title}</h3>
+          <h4 className="youtube__author">{props.author}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
