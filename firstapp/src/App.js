48723297c21e47279ca3ccb13ee8d139c11fe339
leqdung <dzung.dg@gmail.com>
2022-10-14 23:44:36 +0700

    cách gọi 1 component ra đơn giản nhất

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,28 +1,39 @@
-import { Component } from "react";
 import "./App.css";
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+// viết 1 component đơn giản nhất là Feature() rồi đưa nó ra index
 
-  render() {
-    return (
+function Feature() {
+  return (
+    <div>
       <div className="post">
         <img
-          src="https://images.unsplash.com/photo-1542719089-75ed3c483a8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-          alt="image"
+          src="https://images.unsplash.com/photo-1519929125787-88a2485dc4f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          alt="post imagaa"
           className="post__image"
         />
-        <h3 className="post__title">This is awesome title </h3>
-        <p className="post__content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-          earum maiores. Ex libero molestiae possimus temporibus eligendi
-          dolorum sint. Fuga corrupti delectus sed beatae aspernatur blanditiis
-          quidem, odio laudantium perferendis.
-        </p>
+        <div className="post__content">
+          <h3 className="post__title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          </h3>
+          <p className="post__desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            reiciendis magnam nemo nesciunt, repellat asperiores qui vel cumque
+            dolore suscipit fugiat rerum ipsa ut fugit aut vitae iure tempora
+            enim!
+          </p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Feature /> {/**gọi 1 component ra  */}
+      <Feature /> {/**gọi 1 component ra  */}
+      <Feature /> {/**gọi 1 component ra  */}
+    </div>
+  );
 }
 
 export default App;
