977b2babd8a16699375eb2077062648e614d082e
leqdung <dzung.dg@gmail.com>
2022-10-15 00:34:29 +0700

    Hướng dẫn props cơ bản

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,20 @@
-import "./App.css";
+import './App.css';
 // viết 1 component đơn giản nhất là Feature() rồi đưa nó ra index
 
 function Feature() {
   return (
     <div>
-      <div className="post">
+      <div className='post'>
         <img
-          src="https://images.unsplash.com/photo-1519929125787-88a2485dc4f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          alt="post imagaa"
-          className="post__image"
+          src='https://images.unsplash.com/photo-1519929125787-88a2485dc4f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+          alt='post imagaa'
+          className='post__image'
         />
-        <div className="post__content">
-          <h3 className="post__title">
+        <div className='post__content'>
+          <h3 className='post__title'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
           </h3>
-          <p className="post__desc">
+          <p className='post__desc'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
             reiciendis magnam nemo nesciunt, repellat asperiores qui vel cumque
             dolore suscipit fugiat rerum ipsa ut fugit aut vitae iure tempora
@@ -30,29 +30,36 @@ function App() {
   return (
     <div>
       <YoutubeItem
-        image="https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        avatar="https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        title="Title awesom"
-        author="LeQDung"
+        image='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        avatar='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        title='Title awesom'
+        author=''
       />
     </div>
   );
 }
 
-//minh họa props bằng youtube item component
-// mục đích của props là để thay đổi ảnh bìa, tên người, title,avatar ==> props sinh ra để được truyền vào chỗ cần thay đổi trong component
+//minh họa props : dung youtube item component
+// mục đích của props : là để thay đổi ảnh bìa, tên người, title,avatar ==> props sinh ra để được truyền vào chỗ cần thay đổi trong component
+// {data}:  tat cả các dữ liệu đều được lưu trong dấu
 function YoutubeItem(props) {
   console.log(props);
   return (
-    <div className="youtube__item">
-      <div className="youtube__image">
-        <img src={props.image} alt="youtube image" />
+    <div className='youtube__item'>
+      <div className='youtube__image'>
+        <img src={props.image} alt='youtube image' />
       </div>
-      <div className="youtube__footer">
-        <img src={props.avatar} alt="" className="youtube__avata" />
-        <div className="youtube__infor">
-          <h3 className="youtube__title">{props.title}</h3>
-          <h4 className="youtube__author">{props.author}</h4>
+      <div className='youtube__footer'>
+        <img src={props.avatar} alt='' className='youtube__avata' />
+        <div className='youtube__infor'>
+          <h3 className='youtube__title'>
+            {/* Tạo trường hợp nếu trường không có dữ liệu sẽ hiện ra thông báo */}
+            {props.title || 'This is exembail title'}
+          </h3>
+          <h4 className='youtube__author'>
+            {/* Tạo trường hợp nếu truong không có dữ liệu sẽ hiện ra thông báo */}
+            {props.author || 'not found the author'}
+          </h4>
         </div>
       </div>
     </div>
