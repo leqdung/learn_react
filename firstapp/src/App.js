e90496e426d9f8bfe866a8b2f6021a7737cd4e58
leqdung <dzung.dg@gmail.com>
2022-10-14 09:48:41 +0700

    state life circle

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,63 +1,30 @@
+import { Component } from "react";
 import "./App.css";
-import FComment from "./FComment";
-import Comment from "./Comment";
-
-const comments = [
-  {
-    author: "Thor1",
-    content: "this is content 6",
-    timeAgo: "Today is 0:39 pm",
-  },
-
-  {
-    author: "Thor7",
-    content: "this is content 7",
-    timeAgo: "Today is 7:39 pm",
-  },
-  {
-    author: "Thor8",
-    content: "this is content 8",
-    timeAgo: "Today is 2:39 pm",
-  },
-  {
-    author: "Thor9",
-    content: "this is content 9",
-    timeAgo: "Today is 1:39 pm",
-  },
-];
-
-function App() {
-  const fcomment = comments.map((item) => {
-    return (
-      <FComment
-        key={item.id}
-        author={item.author}
-        content={item.content}
-        timeAgo={item.timeAgo}
-      />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errorMessage: "",
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error });
+      }
     );
-  });
-  return (
-    <div className="ui component">
-      {fcomment}
-      <hr />
+  }
 
-      {comments.length <= 0 ? (
-        <p>No comments</p>
-      ) : (
-        comments.map((item) => (
-          <Comment
-            key={item.id}
-            author={item.author}
-            content={item.content}
-            timeAgo={item.timeAgo}
-          />
-        ))
-      )}
-      <hr />
-
-      {comments.length >= 0 && <p>Total of comments : {comments.length}</p>}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {this.state.errorMessage && <h3>{this.state.errorMessage}</h3>}
+        {this.state.latitude && <h3>Latitude: {this.state.latitude}</h3>}
+      </div>
+    );
+  }
 }
+
 export default App;
