42cfc24632f8446e04478da2834ba713d984e187
leqdung <dzung.dg@gmail.com>
2022-10-14 21:05:33 +0700

    jsx

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -3,25 +3,23 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      latitude: null,
-      errorMessage: "",
-    };
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ latitude: position.coords.latitude });
-      },
-      (error) => {
-        this.setState({ errorMessage: error });
-      }
-    );
   }
 
   render() {
     return (
-      <div>
-        {this.state.errorMessage && <h3>{this.state.errorMessage}</h3>}
-        {this.state.latitude && <h3>Latitude: {this.state.latitude}</h3>}
+      <div className="post">
+        <img
+          src="https://images.unsplash.com/photo-1542719089-75ed3c483a8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+          alt="image"
+          className="post__image"
+        />
+        <h3 className="post__title">This is awesome title </h3>
+        <p className="post__content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          earum maiores. Ex libero molestiae possimus temporibus eligendi
+          dolorum sint. Fuga corrupti delectus sed beatae aspernatur blanditiis
+          quidem, odio laudantium perferendis.
+        </p>
       </div>
     );
   }
