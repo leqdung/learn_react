8fd59a26e1bc9ff71ffd3fb0c457cd7aa6e60761
leqdung <dzung.dg@gmail.com>
2022-10-15 10:51:26 +0700

    2 cach style css cho component

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,45 +1,40 @@
 import './App.css';
-// viết 1 component đơn giản nhất là Feature() rồi đưa nó ra index
-
-function Feature() {
-  return (
-    <div>
-      <div className='post'>
-        <img
-          src='https://images.unsplash.com/photo-1519929125787-88a2485dc4f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-          alt='post imagaa'
-          className='post__image'
-        />
-        <div className='post__content'>
-          <h3 className='post__title'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          </h3>
-          <p className='post__desc'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            reiciendis magnam nemo nesciunt, repellat asperiores qui vel cumque
-            dolore suscipit fugiat rerum ipsa ut fugit aut vitae iure tempora
-            enim!
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
-//App là  parent component: trong quan hệ cha con liên quan đến props
+// Viết css cho các thành phần trong component
+// Viết trực tiếp vào function component hoặc viết vào file riêng App.css
+// =========
+// App() là  parent component: trong quan hệ cha con liên quan đến props
 function App() {
   return (
-    <div>
+    <div className='youtube-list'>
       <YoutubeItem
         image='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-        avatar='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-        title='Title awesom'
-        author=''
+        avatar='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+        title='Title awesome'
+        author='leQdung'
+      />
+      <YoutubeItem
+        image='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        avatar='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+        title='Title awesome'
+        author='leQdung'
+      />
+      <YoutubeItem
+        image='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        avatar='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+        title='Title awesome'
+        author='leQdung'
+      />
+      <YoutubeItem
+        image='https://images.unsplash.com/photo-1489176876421-3b720db0fb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        avatar='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+        title='Title awesome'
+        author='leQdung'
       />
     </div>
   );
 }
 
-//minh họa props : dung youtube item component
+// minh họa props : dung youtube item component
 // mục đích của props : là để thay đổi ảnh bìa, tên người, title,avatar ==> props sinh ra để được truyền vào chỗ cần thay đổi trong component
 // {data}:  tat cả các dữ liệu đều được lưu trong dấu
 function YoutubeItem(props) {
